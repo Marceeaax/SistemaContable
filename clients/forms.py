66f2d0cbc5b822712af8b5c8e41a76d63690467f
DIGI600 <DIGI600@ine.gov>
2024-03-19 15:13:17 -0300

    progreso

--- clients/forms.py
+++ clients/forms.py
@@ -8,10 +8,10 @@ class PersonaFisicaForm(forms.ModelForm):
     apellido = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Apellido'}), label='')
     ruc = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'RUC'}), label='')
     contrasena = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Contraseña (Marangatu)'}), label='')
-    telefono = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Teléfono'}), label='')
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email'}), label='')
-    fecha_nacimiento = forms.DateField(widget=forms.TextInput(attrs={'placeholder': 'Fecha de nacimiento'}), label='')
-    direccion = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Dirección'}), label='')
+    telefono = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Teléfono'}), label='', required=False)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email'}), label='', required=False)
+    fecha_nacimiento = forms.DateField(widget=forms.TextInput(attrs={'placeholder': 'Fecha de nacimiento'}), label='', required=False)
+    direccion = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Dirección'}), label='', required=False)
     
     def calcular_cedula(self):
         ruc = self.cleaned_data.get("ruc")
@@ -31,10 +31,10 @@ class PersonaFisicaForm(forms.ModelForm):
 class PersonaJuridicaForm(forms.ModelForm):
     razon_social = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Razón social'}), label='')
     ruc = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'RUC'}), label='')
-    direccion_fiscal = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Dirección fiscal'}), label='')
-    representante_legal = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Representante legal'}), label='')
-    telefono = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Telefono'}), label='')
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email'}), label='')
+    direccion_fiscal = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Dirección fiscal'}), label='', required=False)
+    representante_legal = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Representante legal'}), label='', required=False)
+    telefono = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Telefono'}), label='', required=False)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email'}), label='', required=False)
 
     class Meta:
         model = PersonaJuridica
--- clients/models.py
+++ clients/models.py
@@ -15,16 +15,16 @@ class PersonaFisica(Persona):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     cedula = models.CharField(max_length=8)
-    fecha_nacimiento = models.DateField(blank=True)
+    fecha_nacimiento = models.DateField(null=True)
     direccion = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
 
 class PersonaJuridica(Persona):
-    razon_social = models.CharField(max_length=150)
-    direccion_fiscal = models.TextField()
-    representante_legal = models.CharField(max_length=150)
+    razon_social = models.CharField(max_length=150, blank=True)
+    direccion_fiscal = models.TextField(blank=True)
+    representante_legal = models.CharField(max_length=150, blank=True)
 
     def __str__(self):
         return self.razon_social
