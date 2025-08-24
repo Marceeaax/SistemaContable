81e26658a42bc1867cd1a61a87c607e313635a98
Marceeaax <marcelo.adrian2011@fpuna.edu.py>
2025-08-24 10:50:38 -0300

    Limpieza del repositorio, mantener historial

--- accounts/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Account(models.Model):
-    name = models.CharField(max_length=255)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
-    # ... otros campos necesarios
--- clients/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import clientes_ruc_view, crear_cliente, editar_cliente, datos_faltantes
-
-urlpatterns = [
-    path('clientesruc/', clientes_ruc_view, name='clientesruc'),
-    path('crear/', crear_cliente, name='crear_cliente'),
-    path('editar/', editar_cliente, name='editar_cliente'),
-    path('datos-faltantes/<str:ruc>/', datos_faltantes, name='datos_faltantes')
-]
